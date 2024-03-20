60b92df276880b5cae35e20dc0743aa506723905
EL AMRI HALIMA <elamrihalima1@gmail.com>
2024-03-20 12:32:23 +0000

    changes in config  gatway and visitors work and ville

--- visitor/src/main/java/com/halima/visitor/VisitorApplication.java
+++ visitor/src/main/java/com/halima/visitor/VisitorApplication.java
@@ -2,8 +2,10 @@ package com.halima.visitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class VisitorApplication {
 
 	public static void main(String[] args) {
